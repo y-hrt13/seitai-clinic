0cae2e774bd2487c5010f5d63f456574da65863b
y-hrt13 <y.hrt13@gmail.com>
2025-10-12 22:09:49 +0900

    fix: ロゴ画像のalt属性をてのひらに変更

--- components/header.tsx
+++ components/header.tsx
@@ -13,7 +13,7 @@ export function Header() {
         <div className="flex items-center justify-between">
           <div className="flex flex-col">
             <div className="flex items-center gap-2">
-              <img src="/tenohira-gray.png" alt="ロゴ" className="h-10 w-10" />
+               <img src="/tenohira-gray.png" alt="てのひら" className="h-10 w-10" />
             </div>
             <div className="text-xs text-muted-foreground">10年後も健康で美しくいるために</div>
           </div>
